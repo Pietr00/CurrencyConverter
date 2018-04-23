4d658cd4061f5d492cde9b6f9055afcc427010ca
Pietr00 <nalbertpietro@gmail.com>
2018-04-23 11:10:55 -0300

    Logo

--- app/index.js
+++ app/index.js
@@ -4,8 +4,9 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import Home from './screens/Home';
 
 EStyleSheet.build({
-  $primaryBlue: '#4F6D7A',
+    $primaryBlue: '#4F6D7A',
+    $white: '#FFFFFF'
 });
-
+ 
 export default () => <Home />;
 
